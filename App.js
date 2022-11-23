42b1b3f447adbd9f1ca72b7e80a1803cf9941c81
DJ Bowers <daniel_bowers@icloud.com>
2022-11-23 14:48:19 -0800

    feat(App): implement twrnc and start building app

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, Text, View } from 'react-native'
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex-col items-center`}>
+      <Text>Simple & Sinister</Text>
+      <Button title="Start" />
+      <View style={tw`flex-row`}>
+        <Text>Swings</Text>
+        <Button title="+" />
+      </View>
+      <View style={tw`flex-row`}>
+        <Text>Getups</Text>
+        <Button title="+" />
+      </View>
+      <Button title="Done" />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
