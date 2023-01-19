824130e1ce42d07697539a0549227dac905b6166
DJ Bowers <daniel_bowers@icloud.com>
2023-01-19 11:55:25 -0800

    chore(theme): add some initial theme options

---
+++ src/theme/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '',
+  secondary: '',
+  textPrimary: 'EB7E56',
+  textTertiary: '',
+  textGray: '',
+  borderColor: '',
+  backgroundColor: '#191922',
+  ratingColor: '',
+  progressGray: '',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#F9F9F9',
+};
