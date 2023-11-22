d7e75eaa93b38b38c0d022fb83b1225d17638ddf
DJ Bowers <daniel_bowers@icloud.com>
2023-11-21 18:59:39 -0800

    feat(web): redesign workout history screen

--- apps/web/src/pages/History/HistoryPage.test.jsx
+++ apps/web/src/pages/History/HistoryPage.test.jsx
@@ -1,34 +1,28 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
 
-import { workoutLogs } from '~/mocks/data';
-
 import * as stories from './HistoryPage.stories';
 
 const { Normal } = composeStories(stories);
 
 describe('workout history page', () => {
-  const workoutLog = workoutLogs[0];
-
   beforeEach(() => {
     render(<Normal />);
   });
 
   test('renders name of task', async () => {
-    await screen.findByText(workoutLog.tasks[0], { exact: false });
+    await screen.findAllByText('Clean and Press', { exact: false });
   });
 
   test('renders additional notes', async () => {
-    await screen.findByText(workoutLog.notes, { exact: false });
+    await screen.findAllByText('The Giant', { exact: false });
   });
 
-  test('renders length of workout', async () => {
-    await screen.findByText(`${workoutLog.minutes} Minutes`, { exact: false });
+  test('renders workout density', async () => {
+    await screen.findByText('88.2');
   });
 
-  test('renders rep ladders correctly', async () => {
-    await screen.findByText(`Reps / Round: ${workoutLog.reps.join(', ')}`, {
-      exact: false,
-    });
+  test('renders workout date', async () => {
+    await screen.findByText('11-7 Tue');
   });
 });
