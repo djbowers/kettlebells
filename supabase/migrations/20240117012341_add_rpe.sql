e9377fad966ed15d4fac0a678e5922543720efe3
DJ Bowers <daniel_bowers@icloud.com>
2024-01-24 15:24:09 -0800

    chore: edit migration to rename columns

--- supabase/migrations/20240117012341_add_rpe.sql
+++ supabase/migrations/20240117012341_add_rpe.sql
@@ -1,12 +1,8 @@
 create type "public"."RPE" as enum ('noEffort', 'easy', 'ideal', 'hard', 'maxEffort');
 
-alter table "public"."workout_logs" drop column "reps";
+alter table "public"."workout_logs" rename column "reps" to "rep_scheme";
 
-alter table "public"."workout_logs" drop column "tasks";
-
-alter table "public"."workout_logs" add column "movements" text[] not null;
-
-alter table "public"."workout_logs" add column "rep_scheme" smallint[] not null default '{}'::smallint[];
+alter table "public"."workout_logs" rename column "tasks" to "movements";
 
 alter table "public"."workout_logs" add column "rpe" "RPE" default 'ideal'::"RPE";
 
