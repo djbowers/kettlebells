f7ba492fe82a3072723fbc0f83e1f09b5281b36c
DJ Bowers <daniel_bowers@icloud.com>
2024-03-10 18:58:19 -0700

    style: add shadow to page for larger screen sizes

--- apps/web/src/components/Page.tsx
+++ apps/web/src/components/Page.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 export const Page = ({ children, title = null, actions = null }: Props) => {
   return (
-    <div className="mx-auto my-3 flex max-w-lg flex-col gap-2">
+    <div className="mx-auto my-3 flex max-w-md flex-col gap-2 rounded-xl px-3 py-2 sm:shadow-lg">
       {title && <div className="text-xl font-semibold">{title}</div>}
       {children}
-      {actions && <div className="justify-end">{actions}</div>}
+      {actions && <div className="flex justify-center">{actions}</div>}
     </div>
   );
 };
